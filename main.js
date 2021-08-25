e23dbad15f3512955808bfe36a0c5d585b603153
Dlord <dthomaslord@gmail.com>
2021-08-25 10:57:02 +0300

    Print is working and constructor for Field class in place

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const prompt = require('prompt-sync')({sigint: true});
+// const prompt = require('prompt-sync')({sigint: true});
 
 const hat = '^';
 const hole = 'O';
@@ -6,5 +6,25 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
+    constructor(field) {
+        this.field = field;
+    }
   
-}
+    print() {
+        for(let i = 0; i < this.field.length; i ++) {
+            // console.log(`Row; ${i}`);
+            for(let j = 0; j < this.field[i].length; j++) {
+                // console.log(`j: ${j}`);
+                process.stdout.write(this.field[i][j] + " ");
+                // process.stdout.write(this.field[i][j]);
+            }
+            console.log();
+        }
+    }
+  }
+  
+  
+  const test = new Field([[hole, fieldCharacter], [pathCharacter, hat]]);
+  test.print();
+
+//   use /r to replace character in the game
